cf06fc1a4837a4e9bd0ac49488d1ecfec3bbe599
Stefano Mavilio <st.mavilio@gmail.com>
2022-04-02 23:02:35 +0200

    request coinmarketcap api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,18 @@
 import * as functions from "firebase-functions";
+import axios from "axios";
 
 exports.scheduledFunctionCrontab = functions.pubsub.schedule("*/20 * * * *")
-    .onRun(() => {
-        functions.logger.info("Cronjob works!", {structuredData: true});
+    .onRun(async () => {
+        try {
+            const response = await axios.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=5000", {
+                headers: {
+                    "X-CMC_PRO_API_KEY": "fba02c9b-e35c-4332-bcbb-7ccad47c9607",
+                },
+            });
+            functions.logger.info(response.data);
+        } catch (e) {
+            functions.logger.error(e);
+        }
+
         return null;
     });
