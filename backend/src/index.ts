ad54af2500bae7f0e2add71620487bb831036d58
Stefano Mavilio <st.mavilio@gmail.com>
2022-04-02 22:34:09 +0200

    make eslint work with IntelliJ

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import * as functions from "firebase-functions";
 
-exports.scheduledFunctionCrontab = functions.pubsub.schedule('*/20 * * * *')
-    .onRun((context: any) => {
+exports.scheduledFunctionCrontab = functions.pubsub.schedule("*/20 * * * *")
+    .onRun(() => {
         functions.logger.info("Cronjob works!", {structuredData: true});
         return null;
     });
