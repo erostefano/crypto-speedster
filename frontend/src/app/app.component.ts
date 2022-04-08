1e49ff6a6fe4df3808e0495977f8793fe1a8f91b
Stefano Mavilio <st.mavilio@gmail.com>
2022-04-08 09:47:28 +0200

    add firebase to angular

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { listAll, ref, Storage } from "@angular/fire/storage";
+import { from, map, Observable } from "rxjs";
+import { StorageReference } from "@firebase/storage";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'crypto-speedster';
+  title = "crypto-speedster";
+
+  items: Observable<StorageReference[]>;
+
+  constructor(private storage: Storage) {
+    const storageReference = ref(storage);
+
+    this.items = from(listAll(storageReference)).pipe(
+      map((listResult) => listResult.items)
+    );
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { environment } from "../environments/environment";
+import { getStorage, provideStorage } from "@angular/fire/storage";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideStorage(() => getStorage()),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
