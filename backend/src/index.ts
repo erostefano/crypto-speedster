8031734f9a22c7bdaf776452e30172ac19c10ca5
Stefano Mavilio <st.mavilio@gmail.com>
2022-04-24 12:29:53 +0200

    add endpoint to load ranking

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,10 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import axios from 'axios';
-import { config, url } from './env';
+import { config, rankKey, url } from './env';
+import * as path from 'path';
+import * as os from 'os';
+import * as fs from 'fs';
 
 admin.initializeApp();
 
@@ -19,3 +22,34 @@ exports.scheduledCryptoListingDownload = functions.pubsub
 
         return null;
     });
+
+exports.getRanking = functions.https.onRequest(async (req, resp) => {
+    // TODO: check auth
+    resp.header('Access-Control-Allow-Origin', '*');
+    resp.header('Access-Control-Allow-Headers', '*');
+
+    const queryDate = req.query['date'] as string;
+
+    const bucket = await admin.storage().bucket();
+    const filesResponse = await bucket.getFiles();
+    const files = filesResponse[0];
+    const file = files.find((file) => file.metadata?.name?.includes(queryDate));
+
+    // TODO: return different status code if file not found
+    const fileName = file!.metadata.name;
+    const tempFile = path.join(os.tmpdir(), fileName);
+    await bucket.file(fileName).download({ destination: tempFile });
+
+    const ranking = fs.readFileSync(tempFile, { encoding: 'utf-8' });
+    const coins = JSON.parse(ranking).map(
+        (coin: { cmc_rank: string; name: string }) => {
+            return {
+                rank: coin[rankKey],
+                name: coin.name,
+            };
+        }
+    );
+    resp.send({
+        data: coins,
+    });
+});
