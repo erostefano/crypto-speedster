ea9916cf8715f0c590ab6a2c9a02d11d5b0c87bf
Stefano Mavilio <st.mavilio@gmail.com>
2022-06-23 22:56:40 +0200

    show rest of the table

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Functions, httpsCallable } from '@angular/fire/functions';
 import { from, map, Observable, forkJoin } from 'rxjs';
-import { format, subWeeks } from 'date-fns';
+import { format, subWeeks, subMonths } from 'date-fns';
 
 @Component({
     selector: 'app-root',
@@ -19,6 +19,11 @@ export class AppComponent {
                 today: from(httpsCallable(functions, `getRanking?date=${format(new Date(), 'yyyy-MM-dd')}`)()),
                 oneWeekAgo: from(httpsCallable(functions, `getRanking?date=${format(subWeeks(new Date(), 1), 'yyyy-MM-dd')}`)()),
                 twoWeeksAgo: from(httpsCallable(functions, `getRanking?date=${format(subWeeks(new Date(), 2), 'yyyy-MM-dd')}`)()),
+                oneMonthAgo: from(httpsCallable(functions, `getRanking?date=${format(subMonths(new Date(), 1), 'yyyy-MM-dd')}`)()),
+                twoMonthsAgo: from(httpsCallable(functions, `getRanking?date=${format(subMonths(new Date(), 2), 'yyyy-MM-dd')}`)()),
+                threeMonthsAgo: from(httpsCallable(functions, `getRanking?date=${format(subMonths(new Date(), 3), 'yyyy-MM-dd')}`)()),
+                halfYearAgo: from(httpsCallable(functions, `getRanking?date=${format(subMonths(new Date(), 6), 'yyyy-MM-dd')}`)()),
+                oneYearAgo: from(httpsCallable(functions, `getRanking?date=${format(subMonths(new Date(), 12), 'yyyy-MM-dd')}`)()),
             });
     }
 
