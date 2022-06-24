ee48072fe499c52ecda037ee28cd6a7f60ffb1b5
Stefano Mavilio <st.mavilio@gmail.com>
2022-06-24 21:33:07 +0200

    clean code

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,46 +1,48 @@
 import { Component } from '@angular/core';
 import { Functions, httpsCallable } from '@angular/fire/functions';
-import { from, map, Observable, forkJoin, catchError, of } from 'rxjs';
-import { format, subWeeks, subMonths } from 'date-fns';
+import { catchError, forkJoin, from, map, Observable, of } from 'rxjs';
+import { format, subMonths, subWeeks } from 'date-fns';
 
-const error$ = of({data: []});
+const error$: Observable<string[]> = of([]);
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
     title = 'crypto-speedster';
 
-    rankings$: Observable<any>;
+    rows$: Observable<any>;
 
     constructor(private functions: Functions) {
-        this.rankings$ = 
+        this.rows$ =
             forkJoin({
-                today: from(httpsCallable(functions, `getRanking?date=${format(new Date(), 'yyyy-MM-dd')}`)()).pipe(catchError(_ => error$)),
-                oneWeekAgo: from(httpsCallable(functions, `getRanking?date=${format(subWeeks(new Date(), 1), 'yyyy-MM-dd')}`)()).pipe(catchError(_ => error$)),
-                twoWeeksAgo: from(httpsCallable(functions, `getRanking?date=${format(subWeeks(new Date(), 2), 'yyyy-MM-dd')}`)()).pipe(catchError(_ => error$)),
-                oneMonthAgo: from(httpsCallable(functions, `getRanking?date=${format(subMonths(new Date(), 1), 'yyyy-MM-dd')}`)()).pipe(catchError(_ => error$)),
-                twoMonthsAgo: from(httpsCallable(functions, `getRanking?date=${format(subMonths(new Date(), 2), 'yyyy-MM-dd')}`)()).pipe(catchError(_ => error$)),
-                threeMonthsAgo: from(httpsCallable(functions, `getRanking?date=${format(subMonths(new Date(), 3), 'yyyy-MM-dd')}`)()).pipe(catchError(_ => error$)),
-                halfYearAgo: from(httpsCallable(functions, `getRanking?date=${format(subMonths(new Date(), 6), 'yyyy-MM-dd')}`)()).pipe(catchError(_ => error$)),
-                oneYearAgo: from(httpsCallable(functions, `getRanking?date=${format(subMonths(new Date(), 12), 'yyyy-MM-dd')}`)()).pipe(catchError(_ => error$)),
+                today: from(httpsCallable(functions, `getRanking?date=${format(new Date(), 'yyyy-MM-dd')}`)()).pipe(map(response => response.data as string[]), catchError(_ => error$)),
+                oneWeekAgo: from(httpsCallable(functions, `getRanking?date=${format(subWeeks(new Date(), 1), 'yyyy-MM-dd')}`)()).pipe(map(response => response.data as string[]), catchError(_ => error$)),
+                twoWeeksAgo: from(httpsCallable(functions, `getRanking?date=${format(subWeeks(new Date(), 2), 'yyyy-MM-dd')}`)()).pipe(map(response => response.data as string[]), catchError(_ => error$)),
+                oneMonthAgo: from(httpsCallable(functions, `getRanking?date=${format(subMonths(new Date(), 1), 'yyyy-MM-dd')}`)()).pipe(map(response => response.data as string[]), catchError(_ => error$)),
+                twoMonthsAgo: from(httpsCallable(functions, `getRanking?date=${format(subMonths(new Date(), 2), 'yyyy-MM-dd')}`)()).pipe(map(response => response.data as string[]), catchError(_ => error$)),
+                threeMonthsAgo: from(httpsCallable(functions, `getRanking?date=${format(subMonths(new Date(), 3), 'yyyy-MM-dd')}`)()).pipe(map(response => response.data as string[]), catchError(_ => error$)),
+                halfYearAgo: from(httpsCallable(functions, `getRanking?date=${format(subMonths(new Date(), 6), 'yyyy-MM-dd')}`)()).pipe(map(response => response.data as string[]), catchError(_ => error$)),
+                oneYearAgo: from(httpsCallable(functions, `getRanking?date=${format(subMonths(new Date(), 12), 'yyyy-MM-dd')}`)()).pipe(map(response => response.data as string[]), catchError(_ => error$))
             })
-            .pipe(
-                map(rankings => {
-                    return {
-                        today: rankings.today.data,
-                        oneWeekAgo: rankings.oneWeekAgo.data,
-                        twoWeeksAgo: rankings.twoWeeksAgo.data,
-                        oneMonthAgo: rankings.oneMonthAgo.data,
-                        twoMonthsAgo: rankings.twoMonthsAgo.data,
-                        threeMonthsAgo: rankings.threeMonthsAgo.data,
-                        halfYearAgo: rankings.halfYearAgo.data,
-                        oneYearAgo: rankings.oneYearAgo.data
-                    }                     
-                })
-            );
+                .pipe(
+                    map(rankings => {
+                        return rankings.today.map(rank => {
+                            return {
+                                today: rank,
+                                oneWeekAgo: this.getRank(rankings.oneWeekAgo, rank),
+                                twoWeeksAgo: this.getRank(rankings.twoWeeksAgo, rank),
+                                oneMonthAgo: this.getRank(rankings.oneMonthAgo, rank),
+                                twoMonthsAgo: this.getRank(rankings.twoMonthsAgo, rank),
+                                threeMonthsAgo: this.getRank(rankings.threeMonthsAgo, rank),
+                                halfYearAgo: this.getRank(rankings.halfYearAgo, rank),
+                                oneYearAgo: this.getRank(rankings.oneYearAgo, rank),
+                            }
+                        })
+                    })
+                );
     }
 
     getRank(ranking: string[], coin: string): number {
