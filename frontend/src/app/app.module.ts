e15a6d0d3691d1c707d9edcdc4e7e1f888040722
Stefano Mavilio <st.mavilio@gmail.com>
2022-10-30 22:10:28 +0100

    add loading spinner

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,11 +6,11 @@ import {AppComponent} from "./app.component";
 import {initializeApp, provideFirebaseApp} from "@angular/fire/app";
 import {environment} from "../environments/environment";
 import {getFunctions, provideFunctions} from "@angular/fire/functions";
-import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {MatTableModule} from "@angular/material/table";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 
 @NgModule({
@@ -20,11 +20,11 @@ import {MatButtonModule} from "@angular/material/button";
         AppRoutingModule,
         provideFirebaseApp(() => initializeApp(environment.firebase)),
         provideFunctions(() => getFunctions()),
-        NoopAnimationsModule,
         MatTableModule,
         MatToolbarModule,
         MatIconModule,
         MatButtonModule,
+        MatProgressSpinnerModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
